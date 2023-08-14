d1835f6d06c22bcaa56dffcb7e2e90bf3438a087
Jenni Rivera <jenninrivera@gmail.com>
2023-08-14 13:34:52 -0400

    html css major changes

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-fetch('http://localhost:3000/games')
+fetch('http://localhost:3000/companies')
 .then(response => response.json())
+.then(companies => {
+  companies.forEach(company => {
+    companyNamesOnFactBar(company)
+  }) 
+})
 
+const factBar = document.getElementById('fact-bar')
+
+function companyNamesOnFactBar(company) {
+  const companyName = document.getElementById('companies')
+  companyName.addEventListener('click', () => {
+    randomCommpanyFacts(company)
+  })
+  factBar.appendChild(companyName)
+}
+
+function randomCommpanyFacts(company) {
+  const companyImage = document.getElementById('company-image')
+  companyImage.src = company.image
+
+  const companyFact = document.getElementById('fact-text')
+  companyFact.textContent = company.description
+
+  const factCompanyName = document.getElementById('fact-company-name')
+  factCompanyName = company.name
+}
 let score = 0;
 let health = 1;
 const player = document.getElementById("player");
